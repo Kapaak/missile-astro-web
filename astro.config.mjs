0ceec85c4905e02d124b0ae7d20f38281e7418b4
Pavel Zapletal <pavel.zaplet25@gmail.com>
2024-02-13 10:08:58 +0100

    feat: disable cdn

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,23 @@
-
 import { config as dotenvConfig } from 'dotenv';
-import { defineConfig,  } from "astro/config";
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import { sanityIntegration } from "@sanity/astro";
 import svelte from "@astrojs/svelte";
-
 // Load environment variables
+import vercel from "@astrojs/vercel/serverless";
 dotenvConfig();
 
+// https://astro.build/config
 export default defineConfig({
+  output: "hybrid",
   // image: {
   //   service: passthroughImageService() // to FIX image issue
   // },
   integrations: [tailwind(), sanityIntegration({
     projectId: process.env.SANITY_PROJECT_ID,
     dataset: "production",
-    useCdn: true,
+    useCdn: false,
     apiVersion: "2024-01-07"
-  }), svelte()]
+  }), svelte()],
+  adapter: vercel()
 });
